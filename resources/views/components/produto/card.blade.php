3e5fc266621922d91e4cb1a0f1333f3dd2ae5bc0
wallacemaxters <wallacemaxters@gmail.com>
2025-04-04 13:15:35 -0300

    "Máquinas e Equipamentos" está fazendo o botão desaparecer

--- resources/views/components/produto/card.blade.php
+++ resources/views/components/produto/card.blade.php
@@ -1,7 +1,7 @@
-<x-ui::card {{ $attributes->class('flex-none flex flex-col w-full items-start space-y-4 h-64') }}>
+<x-ui::card {{ $attributes->class('flex-none flex flex-col w-full items-start space-y-3 h-64') }}>
     <x-icon :name="$produto->icone ?? 'heroicon-o-x-mark'" class="lg:w-10 lg:h-10 h-8 w-8 flex-none text-primary-500" />
-    <h3 class="text-xl font-semibold leading-snug">{{ $produto->nome }}</h3>
-    <p class="text-xs lg:text-sm grow">{{ Str::limit($produto->descricao, 95) }}</p>
+    <h3 class="text-xl font-semibold leading-none w-full" title="{{ $produto->nome }}">{{ $produto->nome }}</h3>
+    <p class="text-xs lg:text-sm grow">{{ Str::limit($produto->descricao, 85) }}</p>
     <x-ui::button :variant="['primary', 'small']" :href="$produto->url">
         Saiba mais
     </x-ui::button>
