390f0e44054e5a39acbc6f6d647d86ad5b7a1585
wallacemaxters <wallacemaxters@gmail.com>
2025-04-04 15:08:14 -0300

    remover o botão "Create" de cotnatos

--- app/Filament/Resources/ContatoResource/Pages/ListContatos.php
+++ app/Filament/Resources/ContatoResource/Pages/ListContatos.php
@@ -13,7 +13,7 @@ class ListContatos extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
