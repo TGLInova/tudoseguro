b6845924609326cde7bd78a5c2031f080191d480
wallacemaxters <wallacemaxters@gmail.com>
2025-04-04 14:29:26 -0300

    mudando o "ponto final" de lado

--- resources/views/filament/widgets/posts-agendados-widget.blade.php
+++ resources/views/filament/widgets/posts-agendados-widget.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center gap-2">
             @if($posts)
                 <x-icon name="heroicon-o-clock" class="h-8 text-gray-400" />
-                Você possui {{ $posts === 1 ? 'uma' : $posts }} <strong>{{ $posts === 1 ? 'publicação agendada' : 'publicações agendadas'}}</strong>.
+                Você possui {{ $posts === 1 ? 'uma' : $posts }} <strong>{{ $posts === 1 ? 'publicação agendada' : 'publicações agendadas'}}.</strong>
             @else
                 Não há publicações agendadas.
             @endif
